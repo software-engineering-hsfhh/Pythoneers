8ae3f8dda8078cc96c849249452adb03690665b2
Catalyn211 <72977650+Catalyn211@users.noreply.github.com>
2020-11-05 19:03:51 +0100

    Update GameOver View

--- asteroids.py
+++ asteroids.py
@@ -512,7 +512,6 @@ class GameView(arcade.View):
             coin.remove_from_sprite_lists()
             self.score += 1
 
-
         self.frame_count += 1
 
         if not self.game_over:
@@ -553,6 +552,10 @@ class GameView(arcade.View):
                         self.game_over = True
                         print("Du Lappen hast zu viele Unfälle gebaut... Game Over")
 
+            #for Game Over View
+            if self.game_over:
+                view = GameOverView()
+                self.window.show_view(view)
 
 
 class PauseView(arcade.View):
@@ -603,6 +606,30 @@ class PauseView(arcade.View):
             MyGame = GameView()
             self.window.show_view(game_view)"""
 
+#Game Over View
+class GameOverView(arcade.View):
+    """ View to show when game is over """
+
+    def __init__(self):
+        """ This is run once when we switch to this view """
+        super().__init__()
+        self.texture = arcade.load_texture("GameOver.png")
+
+        # Reset the viewport, necessary if we have a scrolling game and we need
+        # to reset the viewport back to the start so we can see what we draw.
+        arcade.set_viewport(0, SCREEN_WIDTH - 1, 0, SCREEN_HEIGHT - 1)
+
+    def on_draw(self):
+        """ Draw this view """
+        arcade.start_render()
+        self.texture.draw_sized(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,
+                                SCREEN_WIDTH, SCREEN_HEIGHT)
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        """ If the user presses the mouse button, re-start the game. """
+        game_view = GameView()
+        game_view.start_new_game()
+        self.window.show_view(game_view)
 
 def main():
     """ Start the game """
